b5008b326aafe1e187d180d90b2b3628b78edd66
madhu1512 <madhukar.thota@gmail.com>
2016-07-25 12:08:18 -0400

    decoder

---
+++ lua_decoders/http.lua
@@ -0,0 +1,24 @@
+local l = require "lpeg"
+l.locale(l)
+local clf = require "common_log_format"
+
+local msg = {
+    Timestamp   = nil,
+    Type        = msg_type,
+    Payload     = nil,
+    Fields      = nil
+}
+
+function process_message ()
+    
+   local fields = {}
+   local useragent = read_message("Fields[UserAgent]")
+   if useragent then
+                fields.user_agent_browser,
+                fields.user_agent_version,
+                fields.user_agent_os = clf.normalize_user_agent(useragent)
+   end
+   msg.Fields = fields
+    inject_message(msg)
+    return 0
+end
